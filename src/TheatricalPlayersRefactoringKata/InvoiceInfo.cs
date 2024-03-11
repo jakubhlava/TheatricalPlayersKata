e86e47a532f2a99f8db13ee751c0f060c71f4a8e
Jakub Hlava <j.hlava@post.cz>
2024-03-11 16:31:10 +0100

    add invoice info dto

---
+++ src/TheatricalPlayersRefactoringKata/InvoiceInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TheatricalPlayersRefactoringKata;
+
+public class InvoiceInfo
+{
+    public IEnumerable<PlayInfo> PlayInfos { get; set; }
+    
+    public int VolumeCredits { get; set; }
+}
--- src/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ src/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -12,8 +12,10 @@ namespace TheatricalPlayersRefactoringKata
             var totalAmount = 0;
             var volumeCredits = 0;
             var result = string.Format("Statement for {0}\n", invoice.Customer);
-            CultureInfo cultureInfo = new CultureInfo("en-US");
+            
+            var  cultureInfo = new CultureInfo("en-US");
 
+            var infos = new List<PlayInfo>(); 
             foreach(var perf in invoice.Performances) 
             {
                 var play = plays[perf.PlayID];
@@ -44,7 +46,12 @@ namespace TheatricalPlayersRefactoringKata
                 // print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
+                
+                infos.Add(new PlayInfo { Name = play.Name, Price = thisAmount, Seats = perf.Audience });
             }
+            
+            var invoiceInfo = new InvoiceInfo { PlayInfos = infos, VolumeCredits = volumeCredits };
+            
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
             return result;
